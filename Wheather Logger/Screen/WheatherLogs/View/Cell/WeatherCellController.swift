ddbdb14b1e854aac54acb061e2e702f1036edc45
Gokhan Namal <gokhanamal@gmail.com>
2019-11-30 12:19:23 +0100

    initialize app

---
+++ Wheather Logger/Screen/WheatherLogs/View/Cell/WeatherCellController.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherCellController.swift
+//  Wheather Logger
+//
+//  Created by Gokhan Namal on 22.11.2019.
+//  Copyright © 2019 Gokhan Namal. All rights reserved.
+//
+
+import UIKit
+
+class WheatherCell: UITableViewCell {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        self.selectionStyle = .none
+    }
+}
---
+++ Wheather Logger/Screen/WheatherLogs/View/WheatherLogsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  WheatherLogsViewController.swift
+//  Wheather Logger
+//
+//  Created by Gokhan Namal on 22.11.2019.
+//  Copyright © 2019 Gokhan Namal. All rights reserved.
+//
+
+import UIKit
+
+final class WheatherLogsViewController: UIViewController {
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+         super.init(nibName: "WheatherLogsView", bundle: .none)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
